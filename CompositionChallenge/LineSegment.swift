03a052d68eb608543d79d9b02a6145bfd48c4097
ASHLEY RYAN <ashley.ryan19@clcmbp18.local>
2019-04-30 08:33:41 -0500

    Initial Commit

---
+++ CompositionChallenge/LineSegment.swift
@@ -0,0 +1,56 @@
+//
+//  LineSegment.swift
+//  CompositionChallenge
+//
+//  Created by Bob Sagat on 5/7/18.
+//  Copyright (c) 2016 MobileMakers. All rights reserved.
+//
+
+import UIKit
+/***************************************************
+ * Start Your Code Here For MVP
+ ***************************************************/
+
+class LineSegment {
+    
+var start: CGPoint
+var end: CGPoint
+    
+    init(){
+        start = CGPoint(x: 0, y: 0)
+        end = CGPoint(x: 1, y: 1)
+    }
+
+
+init(start: CGPoint, end: CGPoint){
+self.start = start
+self.end = end
+}
+    /***************************************************
+     * Start Your Code Here For Stretch #1
+     ***************************************************/
+
+
+    /***************************************************
+     * End Your Code Here For Stretch #1
+     ***************************************************/
+
+
+
+
+
+    /***************************************************
+     * Start Your Code Here For Stretch #2
+     ***************************************************/
+
+
+    /***************************************************
+     * End Your Code Here For Stretch #2
+     ***************************************************/
+
+
+
+/***************************************************
+ * End Your Code Here For MVP
+ ***************************************************/
+}
---
+++ CompositionChallenge/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  CompositionChallenge
+//
+//  Created by Bob Sagat on 5/7/18.
+//  Copyright (c) 2016 MobileMakers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //Uncomment for MVP
+   var myLine1 = LineSegment()
+    
+    @IBOutlet weak var myTextView: UITextView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let point1 = CGPoint(x: 2, y: 0)
+        let point2 = CGPoint(x: 0, y: 2)
+        
+        //Uncomment the following 4 lines for MVP
+        let myLine2 = LineSegment(start: point1, end: point2)
+        myTextView.text =   "Line 1:\n" +
+                            "  Starts at \(myLine1.start)\n" +
+                            "  Ends at \(myLine1.end)\n" +
+        
+        
+        
+        //Uncomment for Stretch #1
+//                            "  Slope is \(myLine1.getSlope())\n" +
+        //Uncomment for Stretch #2
+//                            "  Length is \(myLine1.getLength())\n\n" +
+        
+        //Uncomment the following 3 lines for MVP
+                            "Line 2:\n" +
+                         "  Starts at \(myLine2.start)\n" +
+        "  Ends at \(myLine2.end)\n"
+        //Uncomment for Stretch #1
+//                          + "  Slope is \(myLine2.getSlope())\n"
+        //Uncomment for Stretch #2
+//                          + "  Length is \(myLine2.getLength())"
+    }
+}
+
